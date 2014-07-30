5cc11152b587f648decfca4e743a1c7404c12969
Avi Aryan <avi.aryan123@gmail.com>
2014-07-30 18:45:50 +0530

    fixed some more minor bugs, added needed keywords

--- components/prism-ahk.js
+++ components/prism-ahk.js
@@ -2,26 +2,28 @@
 // NOTES - follows first-first highlight method, block is locked after highlight, different from SyntaxHl
 Prism.languages.autohotkey= {
 	'comment': /(;[^\n]*|^\s*\/\*[\s\S]*\s*\*\/)/gm,
-	'string': /"(([^"\n\r]|"")*)"/gm,
 	'attr-name':  {
-		pattern: /(\n[ \t]*[a-z]+?(?=[ \t]*?$)|^[ \t]*?[a-z]+?(?=[ \t]+?[^,\(\{\%=]+$))/i, // - 2 steps necessary to block this
+		pattern: /(\n[ \t]*[a-z]+(?=[ \t]*?(\n|$))|^[ \t]*?[a-z]+?(?=[ \t]+?[^,\(\{\%=]+($|\n)))/i, // - 2 steps necessary to block this
 		inside: {
-			'keyword': /\b(else)\b/i
+			'keyword': /\b(else|return|break|continue)\b/i,
+			'property': /\bglobal\b/i
 		}
 		// 2nd part matches single-line or top-line code without commas ( ex- run notepad.exe ) . match before the latter..
-	},
-	'selector':  /(\b(return)\b|^[ \t]*?[a-z]+?(?=([ \t]*?,|[ \t]+?%).*$)|\n[ \t]*?[a-z]+?(?=[ \t]+[^,\(\{=\n]+$))/mi,
+	}, // above string to avoid locking by string and thus matching of single word in case of concat -- eg> var := "abc" highlghted
+	'string': /"(([^"\n\r]|"")*)"/gm,
+	'keyword': /\b(ahk_[a-z]*|if|else|return|for|while|loop|try|catch|throw|new)\b/i,  // TODO: add a regex for global,loc.. if needed
+	'selector':  /(\b(send)\b|^[ \t]*?[a-z]+?(?=([ \t]*?,|[ \t]+?%).*$)|\n[ \t]*?[a-z]+?(?=[ \t]+[^,\(\{=\n]+$))/mi,
+	'property': /\b(global|local|static)\b/i,  // pre-add things that can include commas and thus identified as commands
 	// third part matches without comma commands in block code (start with \n)
-	// return also returns function - an exception.
-	'keyword': /\b(ahk_[a-z]*|if|else|for)\b/i,
+	// Send can send anything including ( and { so hardcode
 	'function': /[^\(\); \t\,\n\+\*\-\=\?>:\\\/<\&%\[\]]+?(?=\()/gm,  //function - don't use .*\) in the end bcoz string locks it
 	'symbol': /^[ \t]*[a-z0-9]+?(?=::)/gm, //hot-keys - label
 	'tag': /^[ \t]*[^\s:]+?(?=:)/gm,  //labels
 	'variable': /\%\w+\%/g,
 	'number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?)\b/g,
-	'operator': /[-+]{1,2}|!|<=?|>=?|={1,3}|&{1,2}|\|?\||\?|\*|\/|\~|\^|\%/g,
+	'operator': /[\+\-\*\\\/:=\?]/g,
 	'punctuation': /[\{}[\]\(\):]/g,
 	'boolean': /\b(true|false)\b/g,
-	'constant': /\b(a_[a-z]*|clipboard|clipboardall|comspec|errorlevel)\b/ig,
+	'constant': /\b(a_[a-z]*|clipboard[a-z]*|comspec|errorlevel)\b/ig,
 	'important': /#\b([a-z]*)\b/ig
 };
